cf7ef850767203ef06d6e73f1c601de48a028e66
frosinastojanovska <stojanovska.frose@gmail.com>
2017-03-24 02:46:48 +0100

    Fix documentation for translation function

--- R/translation.R
+++ R/translation.R
@@ -1,4 +1,7 @@
-#' Finds all protein sequences for the given DNA sequence
+#' Finds all protein sequences for the given DNA sequence. First the algorithm finds the RNA for the given
+#' DNA sequence. Then it starts from the begining searching for a start codon, and stops when finds stop
+#' codon of end of sequence.When one sequence is found, the algorithm searches for new start codon in the
+#' sequence, after the previous start codon.
 #'
 #' @param sequence A vector of characters in lower or upper case indicating some DNA sequence.
 #' @return A list with vectors indicating all possible protein sequences for given DNA.
