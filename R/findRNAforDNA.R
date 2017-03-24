571f43da7cb038663dfc6dc34e8d633473769292
frosinastojanovska <stojanovska.frose@gmail.com>
2017-03-24 01:09:19 +0100

    Upgrade function findRNAforDNA

--- R/findRNAforDNA.R
+++ R/findRNAforDNA.R
@@ -1,12 +1,20 @@
 #' Finds the mRNA sequence for given DNA sequence
 #'
 #' @param dna A vector of characters in lower or upper case indicating some DNA sequence.
+#' @param strandDirection A string indicating the DNA strand direction. It could be "5'->3'", or "3'->5'".
 #' @return Vector of characters reperesenting mRNA sequence for given DNA.
 #' @examples
-#' findRNAforDNA(c("A","C","A","C","A","C","T","A"))
-#' findRNAforDNA(c("a","g","c","a","c","a","c","a"))
+#' findRNAforDNA(c("A","C","A","C","A","C","T","A"), "3'->5'")
+#' findRNAforDNA(c("a","g","c","a","t","a","c","a"), "5'->3'")
 
-findRNAforDNA <- function(dna){
+findRNAforDNA <- function(dna, strandDirection = c("5'->3'", "3'->5'")){
+
+  if(strandDirection != "5'->3'" && strandDirection !=  "3'->5'")
+    return("Not valid strand direction.")
+
+  if(strandDirection == "3'->5'")
+    dna <- getComplementaryStrand(dna)
+
   rna <- c()
   for(i in 1:length(dna)){
     if(dna[i] == 'T'){
@@ -19,5 +27,6 @@ findRNAforDNA <- function(dna){
       rna[i] <- dna[i]
     }
   }
+
   return(rna)
 }
